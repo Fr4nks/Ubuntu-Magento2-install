799ec30ad1e26c489076cdd43a63bdeda7545747
Fr4nks <fsdevilliers@gmail.com>
2017-05-19 14:24:28 +0200

    Second commit

--- install.py
+++ install.py
@@ -1,29 +1,28 @@
 #-*-coding:utf-8-*-
+import sys
+sys.path.insert(0, '/modules')
 from __future__ import print_function #check, don't know might be part of pick.
 from pick import pick #select lists
 import subprocess #run the bash scripts in python
-from yesorno import query_yes_no #runs the yes on no question
-from findreplaceadd import find_replace_add
+from findreplaceadd import find_replace_add #find and replaces or adds
 
-swappage = query_yes_no("Do you even want this fucking swap memory?", None)
-if swappage is True:
-  title = ("How much swap/paging space do you need for your instance")
-  options = ['512', '1024', '2048', '4096', 'Exit']
-  option, index = pick(options, title, indicator='=>', default_index=2)
-  subprocess.call("echo 'test'" +option+ "'more test'", shell=True)
 
-php_install = query_yes_no("Install Php?", None)
-if php_install is True:
-  title = ("Install Php 7 or Php 5")
-  options = ['Php 7', 'Php 5', 'Exit']
-  option, index = pick(options, title, indicator='=>', default_index=1) 
-  if option == 'Php 7':
-    subprocess.call("echo 'shell installs php 7'", shell=True)
-  if option == 'Php 5': 
-    subprocess.call("echo 'shell isntalls php 5'", shell=True)
+title = ("How much swap/paging space do you need for your instance")
+options = ['512', '1024', '2048', '4096', 'Skip >>']
+option, index = pick(options, title, indicator='=>', default_index=1)
+subprocess.call("echo 'test'" +option+ "'more test'", shell=True)
 
-php_ini = query_yes_no("set php.ini file", None)
-if php_install is True:
-  find = "hi"
-  replace = "Fucking hope this doesn't work"
-  find_replace_add('yourBigFile.txt', find, replace)
+title = ("Install PHP")
+options = ['Php 7', 'Php 5', 'Skip >>']
+option, index = pick(options, title, indicator='=>', default_index=1) 
+if option == 'Php 7':
+  subprocess.call("echo 'shell installs php 7'", shell=True)
+if option == 'Php 5': 
+  subprocess.call("echo 'shell isntalls php 5'", shell=True)
+
+title = ("Update the php.ini file")
+options = ['yes', 'no' ]
+options, index = pick(options, title, indicator='=>', default_index=1)
+find = "hi"
+replace = "Hope this doesn't work"
+find_replace_add('yourBigFile.txt', find, replace)
--- modules/findreplaceadd.py
+++ modules/findreplaceadd.py
@@ -1,9 +1,14 @@
 def find_replace_add(file_current, find, replace):
   print (file_current)
   file_old = open(file_current, 'r')
-  file_new = open(file_current + '.tmp', 'w')
-  for line in file_old:
-      if file_new.write(line.replace( find, replace)) is False:
-        file_new.write(replace)
+  file_new = open(file_current + '.tmp', 'w') 
+  lines = file_old.read()
+  found = lines.find(find)
+  if found is True:
+    for line in file_old:
+      file_new.write(line.replace( find, replace))
+  else:
+    file_new.write(replace)  
+
   file_old.close()
   file_new.close()  
