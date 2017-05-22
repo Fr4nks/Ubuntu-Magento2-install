9212ad452d18abc364e870126f34f4bfe1f70c5f
Fr4nks <fsdevilliers@gmail.com>
2017-05-22 17:25:56 +0200

    function resolved

--- findreplaceadd.py
+++ findreplaceadd.py
@@ -1,14 +1,11 @@
-def find_replace_add(file_current, find, replace):
-  print (file_current)
-  file_old = open(file_current, 'r')
-  file_new = open(file_current + '.tmp', 'w') 
-  lines = file_old.read()
-  found = lines.find(find)
-  if found is True:
-    for line in file_old:
-      file_new.write(line.replace( find, replace))
-  else:
-    file_new.write(replace)  
-
-  file_old.close()
-  file_new.close()  
+def find_replace_add(file_current, find_this, replace_that):
+  with open(file_current, 'rb') as file_old:
+    with open(file_current + '.tmp', 'r+') as file_new:
+      found = 0  
+      for line in file_old:
+        file_new.write(line.replace(find_this, replace_that))
+        if find_this in line:
+          found += 1
+      if found == 0:
+          with open(file_current, 'a') as file_append:
+            file_append.write("\n" + replace_that)
--- install.py
+++ install.py
@@ -1,7 +1,7 @@
 #-*-coding:utf-8-*-
-import sys
-sys.path.insert(0, '/modules')
 from __future__ import print_function #check, don't know might be part of pick.
+import sys
+sys.path.insert(1, '/modules')
 from pick import pick #select lists
 import subprocess #run the bash scripts in python
 from findreplaceadd import find_replace_add #find and replaces or adds
@@ -9,20 +9,29 @@ from findreplaceadd import find_replace_add #find and replaces or adds
 
 title = ("How much swap/paging space do you need for your instance")
 options = ['512', '1024', '2048', '4096', 'Skip >>']
-option, index = pick(options, title, indicator='=>', default_index=1)
+option, index = pick(options, title, indicator='=>', default_index=0)
 subprocess.call("echo 'test'" +option+ "'more test'", shell=True)
 
 title = ("Install PHP")
 options = ['Php 7', 'Php 5', 'Skip >>']
-option, index = pick(options, title, indicator='=>', default_index=1) 
-if option == 'Php 7':
+install_php = pick(options, title, indicator='=>', default_index=0) 
+if install_php == 'Php 7':
   subprocess.call("echo 'shell installs php 7'", shell=True)
-if option == 'Php 5': 
+if install_php == 'Php 5': 
   subprocess.call("echo 'shell isntalls php 5'", shell=True)
 
 title = ("Update the php.ini file")
 options = ['yes', 'no' ]
-options, index = pick(options, title, indicator='=>', default_index=1)
-find = "hi"
-replace = "Hope this doesn't work"
-find_replace_add('yourBigFile.txt', find, replace)
+update_php_ini, index = pick(options, title, indicator='=>', default_index=0)
+if update_php_ini == 'yes':
+  find_this = "hello"
+  replace_that = "Hope this still doesn't work test 2"
+  find_replace_add('yourBigFile.txt', find_this, replace_that)
+
+title = ("Second the  php.ini file")
+options = ['yes', 'no' ]
+secondupdate_php_ini, index = pick(options, title, indicator='=>', default_index=0)
+if secondupdate_php_ini == 'yes':
+  find_this = "hello"
+  replace_that = "Append this text 2"
+  find_replace_add('notFoundBigFile.txt', find_this, replace_that)
